072e2f5486532fa82035ce66f6fb91e11b5cf629
samuelslol <56693172+samuelslol@users.noreply.github.com>
2024-11-02 19:20:29 -0500

    Update sayoscript.js

--- sayoscript.js
+++ sayoscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Samu's Script (BETA)
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.3
 // @icon         https://i.imgur.com/L0yl2aC.jpg
 // @description  Hide the advertisement section, error banners, and customize UI in the game <3
 // @discord      samusayo
